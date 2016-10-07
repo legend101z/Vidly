d008c573d5358e74c3b1ddae8b2823dc9c20db17
danny <danny_ramdayal@live.ca>
2016-10-07 17:13:44 -0400

    added movie crud api, added pagenation, sort, filtering using plugin datatables added CustomerMembership DTO

---
+++ Vidly/Vidly/Dtos/MovieDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+
+namespace Vidly.Dtos
+{
+    public class MovieDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        [Required]
+        public byte GenreId { get; set; }
+  
+        public DateTime ReleaseDateTime { get; set; }
+        public DateTime DateAddedTime { get; set; }
+
+        [Range(1, 20)]
+        public int NumberInStock { get; set; }
+    }
+}
